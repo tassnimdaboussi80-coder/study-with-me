f2c6d56fe32b34b418e04d420bbe9b4f92130db9
tassnimdaboussi80-coder <tassnimdaboussi80@gmail.com>
2026-07-31 14:45:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Study With Me is ready 🎀");
+function enterRoom(){
+
+document.querySelector(".welcome").style.display="none";
+
+document.getElementById("room").style.display="block";
+
+}
